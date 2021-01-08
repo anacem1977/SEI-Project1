8545d9afee405025d3e28b78f4d4681751d5bceb
anace-m <maldonadoanaceciliac@johndeere.com>
2021-01-08 13:55:57 -0600

    set up basic structure

--- trivia_app.js
+++ trivia_app.js
@@ -1,1 +1,20 @@
 console.log("Vamos a jugar!");
+
+let questionsDataBase = [
+    {
+        question: "What animal is on México's flag?",
+        optA: "Eagle",
+        optB: "Cow",
+        optC: "Dog",
+        correct: 1
+    },
+    {
+        question: "When does México celebrates Independence Day?",
+        optA: "May 5th",
+        optB: "September 16th",
+        optC: "November 20th",
+        correct: 2
+    }
+]
+
+console.log(questionsDataBase);
