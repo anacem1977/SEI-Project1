8df4a593f899106c2630ac5d8ec67dd49534c488
anace-m <maldonadoanaceciliac@johndeere.com>
2021-01-08 14:18:47 -0600

    define basics of what the code should do

--- trivia_app.js
+++ trivia_app.js
@@ -17,4 +17,20 @@ let questionsDataBase = [
     }
 ]
 
+const startBtn = document.querySelector("#start");
+//event listener para el botón de start
+//cuando se presione start se debe cambiar el título y las instruccionesy desaparecer el botón.
+
+//debe ir moviendose a través del array conforme se vaya respondiendo y registrando el score
+document.querySelector("#question").innerText = questionsDataBase[0].question;
+document.querySelector("#answer1").innerText = questionsDataBase[0].optA;
+document.querySelector("#answer2").innerText = questionsDataBase[0].optB;
+document.querySelector("#answer3").innerText = questionsDataBase[0].optC;
+
+const submitBtn = document.querySelector("#submitAnsr");
+//even listener para el boton SUBMIT
+
+const exitBtn = document.querySelector("#exit");
+//even listener para el boton EXIT
+
 console.log(questionsDataBase);
