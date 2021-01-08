a60005b1c8c2ec0fec8f47517a38b35b0e06780a
anace-m <maldonadoanaceciliac@johndeere.com>
2021-01-08 15:40:33 -0600

    included code for logic testing

--- trivia_app.js
+++ trivia_app.js
@@ -6,14 +6,14 @@ let questionsDataBase = [
         optA: "Eagle",
         optB: "Cow",
         optC: "Dog",
-        correct: 1
+        correct: "A"
     },
     {
         question: "When does México celebrates Independence Day?",
         optA: "May 5th",
         optB: "September 16th",
         optC: "November 20th",
-        correct: 2
+        correct: "B"
     }
 ]
 
@@ -23,27 +23,51 @@ const startBtn = document.querySelector("#start");
 
 //el  siguiente código debe ir moviéndose a través del array conforme se vaya respondiendo y registrando el score
 document.querySelector("#question").innerText = questionsDataBase[0].question;
-document.querySelector("#answer1").innerText = questionsDataBase[0].optA;
-document.querySelector("#answer2").innerText = questionsDataBase[0].optB;
-document.querySelector("#answer3").innerText = questionsDataBase[0].optC;
-
-//INPUT FIELD (LA RESPUESTA SE GRABA EN LA VARIABLE)
-const answerInput = document.querySelector("#yourAnswer");
+document.querySelector("#answer1").innerText = "A. "+ questionsDataBase[0].optA;
+document.querySelector("#answer2").innerText = "B. "+ questionsDataBase[0].optB;
+document.querySelector("#answer3").innerText = "C. "+ questionsDataBase[0].optC;
 
 //BOTÓN SUBMIT
 const submitBtn = document.querySelector("#submitAnsr");
 submitBtn.addEventListener("click", checkAnswer);
 submitBtn.addEventListener("enter", checkEnter);
+//no funciona con ENTER :(
+
+document.querySelector("#yourAnswer").addEventListener("enter", checkEnter)
 
 //una vez que se valide la respuesta, las preguntas y respuestas se deben reemplazar por las siguientes
 
 const exitBtn = document.querySelector("#exit");
-//PENDIENTE: event listener para el boton EXIT
+//PENDIENTE: event listener para el boton EXIT y código para salir
 
 function checkEnter(event) {
+    console.log(event);
     if (event.keyCode === 13) {
         checkAnswer(event)
     }
+}
+
+function checkAnswer(event) {
+    event.preventDefault();
+    const answerInput = document.querySelector("#yourAnswer").value;
+    const capAnswerInput = answerInput.toUpperCase();
+    console.log(capAnswerInput);
+
+    if (capAnswerInput !== "A" && capAnswerInput !== "B" && capAnswerInput !== "C") {
+        document.querySelector("#responseMsg").innerText = capAnswerInput + " is not a valid option"
+    } else {
+    document.querySelector("#yourAnswer").value = "";
+    if (capAnswerInput === questionsDataBase[0].correct) {
+        console.log("correct answer!")
+        document.querySelector("#responseMsg").innerText = "That is correct!";
+    } else {
+        console.log("wrong answer");
+        document.querySelector("#responseMsg").innerText = "That is incorrect!"
+        //LIMPIAR
+        //SI LA RESPUESTA ES INCORRECTA PASAR A LA SIGUIENTE PREGUNTA
+    }
+}
+    //LLEVAR REGISTRO DEL SCORE
 }
 
 console.log(questionsDataBase);
