20992c1d17fa73230508939fa7bc1f4376ca628b
anace-m <maldonadoanaceciliac@johndeere.com>
2021-01-09 12:28:07 -0600

    added buttons for control

--- trivia_app.js
+++ trivia_app.js
@@ -14,18 +14,20 @@ let questionsDataBase = [
         optB: "September 16th",
         optC: "November 20th",
         correct: "B"
+    },
+    {
+        question: "What is Mexico's official name (in English)",
+        optA: "Republic of Mexico",
+        optB: "Mexico",
+        optC: "United Mexican States",
+        correct: "C"
     }
 ]
 
+//BOTÓN START
 const startBtn = document.querySelector("#start");
-//PENDIENTE: event listener para el botón de start
+startBtn.addEventListener("click", startGame);
 //cuando se presione start se debe cambiar el título y las instruccionesy desaparecer el botón.
-
-//el  siguiente código debe ir moviéndose a través del array conforme se vaya respondiendo y registrando el score
-document.querySelector("#question").innerText = questionsDataBase[0].question;
-document.querySelector("#answer1").innerText = "A. "+ questionsDataBase[0].optA;
-document.querySelector("#answer2").innerText = "B. "+ questionsDataBase[0].optB;
-document.querySelector("#answer3").innerText = "C. "+ questionsDataBase[0].optC;
 
 //BOTÓN SUBMIT
 const submitBtn = document.querySelector("#submitAnsr");
@@ -33,12 +35,15 @@ submitBtn.addEventListener("click", checkAnswer);
 submitBtn.addEventListener("enter", checkEnter);
 //no funciona con ENTER :(
 
-document.querySelector("#yourAnswer").addEventListener("enter", checkEnter)
-
-//una vez que se valide la respuesta, las preguntas y respuestas se deben reemplazar por las siguientes
-
+//BOTÓN EXIT
 const exitBtn = document.querySelector("#exit");
 //PENDIENTE: event listener para el boton EXIT y código para salir
+
+//BOTÓN NEXT
+const nextBtn = document.querySelector("#next");
+//PENDIENTE: event listener para el boton NEXT y código para salir
+
+//una vez que se valide la respuesta, las preguntas y respuestas se deben reemplazar por las siguientes
 
 function checkEnter(event) {
     console.log(event);
@@ -47,25 +52,39 @@ function checkEnter(event) {
     }
 }
 
+let yourScore = 0;
+let ignorance = 0;
+
+//el  siguiente código debe ir moviéndose a través del array conforme se vaya respondiendo y registrando el score
+document.querySelector("#question").innerText = questionsDataBase[0].question;
+document.querySelector("#answer1").innerText = "A. "+ questionsDataBase[0].optA;
+document.querySelector("#answer2").innerText = "B. "+ questionsDataBase[0].optB;
+document.querySelector("#answer3").innerText = "C. "+ questionsDataBase[0].optC;
+
 function checkAnswer(event) {
     event.preventDefault();
     const answerInput = document.querySelector("#yourAnswer").value;
     const capAnswerInput = answerInput.toUpperCase();
     console.log(capAnswerInput);
-
     if (capAnswerInput !== "A" && capAnswerInput !== "B" && capAnswerInput !== "C") {
-        document.querySelector("#responseMsg").innerText = capAnswerInput + " is not a valid option"
+        document.querySelector("#responseMsg").innerText = capAnswerInput + " is not a valid option";
+        document.querySelector("#yourAnswer").value = "";
     } else {
+    document.querySelector("#responseMsg").innerText = "";
     document.querySelector("#yourAnswer").value = "";
     if (capAnswerInput === questionsDataBase[0].correct) {
-        console.log("correct answer!")
+        yourScore += 1;
+        console.log(yourScore);
         document.querySelector("#responseMsg").innerText = "That is correct!";
     } else {
-        console.log("wrong answer");
-        document.querySelector("#responseMsg").innerText = "That is incorrect!"
+        ignorance += 1;
+        console.log(ignorance);
+        document.querySelector("#responseMsg").innerText = "That is not correct!"
         //LIMPIAR
         //SI LA RESPUESTA ES INCORRECTA PASAR A LA SIGUIENTE PREGUNTA
+        //BOTON NEXT
     }
+    
 }
     //LLEVAR REGISTRO DEL SCORE
 }
